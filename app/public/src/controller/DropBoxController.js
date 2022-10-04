823f8a4a7c7c1b139b368db77a5b2e8e43a23b92
v1n1c1u5ouza <viniciusdesouzamarinho@gmail.com>
2022-10-04 09:12:08 -0300

    Add: New folder function

--- app/public/src/controller/DropBoxController.js
+++ app/public/src/controller/DropBoxController.js
@@ -1,5 +1,10 @@
 class DropBoxController {
     constructor() {
+
+      this.currentFolder = ['hcode'];
+
+      this.onselectionchange = new Event("selectionchange");
+
       this.onselectionchange = new Event('selectionchange');
       this.btnSendFileEl = document.querySelector("#btn-send-file");
       this.inputFilesEl = document.querySelector("#files");
@@ -56,6 +61,18 @@ class DropBoxController {
     }
   
     initEvents() {
+
+      this.btnNewFolder.addEventListener('click', e => {
+        let name = prompt('Nome da nova pasta:');
+  
+        if (name) {
+          this.getFirebaseRef().push().set({
+            name, 
+            type: 'folder',
+            path: this.currentFolder.join('/')
+          })
+        }
+      })  
 
       this.btnDelete.addEventListener("click", (e) => {
         this.removeTask()
