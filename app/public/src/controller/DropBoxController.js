1d20f9ddecf834cb3691b10a495fe4afd12695c3
v1n1c1u5ouza <viniciusdesouzamarinho@gmail.com>
2022-10-04 09:26:36 -0300

    Add: Navegate folders functions

--- app/public/src/controller/DropBoxController.js
+++ app/public/src/controller/DropBoxController.js
@@ -5,6 +5,7 @@ class DropBoxController {
 
       this.onselectionchange = new Event("selectionchange");
 
+      this.navEl = document.querySelector('#browse-location');
       this.onselectionchange = new Event('selectionchange');
       this.btnSendFileEl = document.querySelector("#btn-send-file");
       this.inputFilesEl = document.querySelector("#files");
@@ -21,6 +22,8 @@ class DropBoxController {
       this.connectFirebase();
       this.initEvents();
       this.readFiles();
+
+      this.openFolder();
     }
   
     connectFirebase() {
@@ -155,9 +158,12 @@ class DropBoxController {
       this.btnSendFileEl.disabled = false
     }
   
-    getFirebaseRef() {
-      return firebase.database().ref('files')
-    }
+    getFirebaseRef(path) {
+
+      if (!path) path = this.currentFolder.join('/')
+  
+      return firebase.database().ref(path);
+    } 
   
     modalShow(show = true) {
       this.snackModalEl.style.display = (show) ? "block" : "none"
@@ -451,18 +457,94 @@ class DropBoxController {
     }
   
     readFiles() {
+
+      this.lastFolder = this.currentFolder.join('/');
+
       this.getFirebaseRef().on('value', snapshot => {
         this.listFilesEl.innerHTML = '';
         snapshot.forEach(snapshotItem => {
           let key = snapshotItem.key;
           let data = snapshotItem.val()
           
-          this.listFilesEl.appendChild(this.getFileView(data, key))
+          if (data.type) {
+            this.listFilesEl.appendChild(this.getFileView(data, key));
+          }
+        });
+      });
+    }
+
+    openFolder() {
+
+      if (this.lastFolder) this.getFirebaseRef(this.lastFolder).off('value');
+  
+      this.renderNav();
+      this.readFiles();
+  
+    }
+  
+    renderNav() {
+  
+      let nav = document.createElement('nav')
+      let path = [];
+  
+      for (let i = 0; i < this.currentFolder.length; i++) {
+        let folderName= this.currentFolder[i];
+        let span = document.createElement('span');
+  
+        path.push(folderName);
+  
+        if ((i+1) === this.currentFolder.length) {
+  
+          span.innerHTML = folderName;
+  
+        } else {
+          span.className = 'breadcrumb-segment__wrapper'
+          span.innerHTML = `
+            <span class="ue-effect-container uee-BreadCrumbSegment-link-0">
+              <a href="#" data-path="${path.join('/')}" class="breadcrumb-segment">${folderName}</a>
+            </span>
+            <svg width="24" height="24" viewBox="0 0 24 24" class="mc-icon-template-stateless" style="top: 4px; position: relative;">
+              <title>arrow-right</title>
+              <path d="M10.414 7.05l4.95 4.95-4.95 4.95L9 15.534 12.536 12 9 8.464z" fill="#637282" fill-rule="evenodd"></path>
+            </svg>
+          `
+        }
+  
+        nav.appendChild(span);
+      }
+  
+      this.navEl.innerHTML = nav.innerHTML;
+  
+      this.navEl.querySelectorAll('a').forEach(a => {
+        a.addEventListener('click', e => {
+          e.preventDefault();
+  
+          this.currentFolder = a.dataset.path.split('/');
+  
+          this.openFolder()
         })
       })
+  
     }
 
     initEventsLi(li) {
+
+      li.addEventListener('dblclick', e => {
+
+        let file = JSON.parse(li.dataset.file);
+  
+        switch (file.type) {
+          case 'folder':
+            this.currentFolder.push(file.name)
+            this.openFolder()
+            break;
+  
+          default:
+            window.open('/file?path=' + file.path)
+        }
+  
+      })
+
         li.addEventListener('click', e => {
 
           this.listFilesEl.dispatchEvent(this.onselectionchange)
