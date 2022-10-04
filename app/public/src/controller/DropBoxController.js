cb5382663b8807a8f872a1e76eeac4ec622761fe
v1n1c1u5ouza <viniciusdesouzamarinho@gmail.com>
2022-10-04 09:07:09 -0300

    Add: Delete file function

--- app/public/src/controller/DropBoxController.js
+++ app/public/src/controller/DropBoxController.js
@@ -35,8 +35,45 @@ class DropBoxController {
     getSelection() {
       return this.listFilesEl.querySelectorAll('.selected');
     }
+
+    removeTask() {
+      let promises = [];
+  
+      this.getSelection().forEach((li) => {
+        let file = JSON.parse(li.dataset.file);
+        let key = li.dataset.key;
+  
+        let formData = new FormData()
+  
+        formData.append('path', file.path);
+        formData.append('key', key);
+  
+        promises.push(this.ajax('/file', 'DELETE', formData));
+  
+      });
+      
+      return Promise.all(promises);
+    }
   
     initEvents() {
+
+      this.btnDelete.addEventListener("click", (e) => {
+        this.removeTask()
+          .then((responses) => {
+  
+            responses.forEach(response => {
+              if (response.fields.key) {
+                this.getFirebaseRef().child
+                (response.fields.key).remove();
+              }
+            })
+  
+            console.log("responses");
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      });
 
       this.btnRename.addEventListener('click', e => {
         let li = this.getSelection()[0];
@@ -107,6 +144,38 @@ class DropBoxController {
   
     modalShow(show = true) {
       this.snackModalEl.style.display = (show) ? "block" : "none"
+    }
+
+    ajax(
+      url,
+      method = "GET",
+      formData = new FormData(),
+      onprogress = function () {},
+      onloadstart = function () {}
+    ) {
+      return new Promise((resolve, reject) => {
+        let ajax = new XMLHttpRequest();
+  
+        ajax.open(method, url);
+  
+        ajax.onload = (event) => {
+          try {
+            resolve(JSON.parse(ajax.responseText));
+          } catch (e) {
+            reject(e);
+          }
+        };
+  
+        ajax.onerror = (event) => {
+          reject(event);
+        };
+  
+        ajax.upload.onprogress = onprogress;
+  
+        onloadstart();
+  
+        ajax.send(formData);
+      });
     }
   
     uploadTask(files) {
